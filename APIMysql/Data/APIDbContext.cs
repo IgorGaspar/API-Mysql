4d9ee2a2f49a67b3abb4914b068c41711dd9c70c
Igor Gaspar <igor.elvis@hotmail.com>
2023-02-12 21:52:14 -0300

    Criada API WEB

---
+++ APIMysql/Data/APIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APIMysql.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIMysql.Data
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Estado> Estado { get; set; }
+    }
+}
---
+++ APIMysql/Models/Estado.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIMysql.Models
+{
+    public class Estado
+    {
+        [Key]
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "O campo Sigla deve ter 2 caracteres")]
+        public string Sigla { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome e obrigatorio")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "O campo Nome deve ter entre 3 e 200 caracteres")]
+        public string Nome { get; set; }
+
+    }
+}
